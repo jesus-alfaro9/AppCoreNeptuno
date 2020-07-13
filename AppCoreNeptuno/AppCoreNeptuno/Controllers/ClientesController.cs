9431c84eba1c2004982f28476ae88fd8b0c11b03
Jesus <jesus.test09@gmail.com>
2020-07-13 03:08:54 -0500

    listar clientes

---
+++ AppCoreNeptuno/AppCoreNeptuno/Controllers/ClientesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppCoreNeptuno.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCoreNeptuno.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ApplicationDBContext context;
+        public ClientesController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(context.Clientes.ToList());
+        }
+    }
+}
---
+++ AppCoreNeptuno/AppCoreNeptuno/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCoreNeptuno.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public string IdCliente { get; set; }
+        public string NombreCompañía { get; set; }
+        public string Dirección { get; set; }
+        public string Ciudad { get; set; }
+        public string País { get; set; }
+        public string Teléfono { get; set; }
+
+    }
+}
